6679c86d9346b1cb6b3f55aa3c58f8ea7c4563ce
sylvan-li <76680992+sylvan-li@users.noreply.github.com>
2023-10-22 22:15:06 -0700

    js file for block21

---
+++ index.js
@@ -0,0 +1,27 @@
+// constructor function called "Car"
+class Car {
+    constructor(make, model, year) {
+      this.make = make;
+      this.model = model;
+      this.year = year;
+    }
+    //adding a method that returns info on the car
+    getDescription() {
+      return `The make of this car is ${this.make}, the model is ${this.model} , and is made in ${this.year}.`;
+    }
+}
+//defining a subclass of "car" with ElectricCar
+class ElectricCar extends Car {
+  constructor(make, model, year, range) {
+    super(make, model, year);
+    this.range = range;
+  }
+  //overriding getDescription method of car
+  getInfo(){
+    return `${super.getDescription()} It has a range of  ${this.range} miles per charge .`;
+  }
+}
+//Creating an instance of ElectricCar with Tesla 
+const Tesla = new ElectricCar("Tesla", "Model S", 2019,300);
+console.log(Tesla);
+console.log(Tesla.getInfo());
